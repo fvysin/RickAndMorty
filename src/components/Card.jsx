fdb2ed7b3934c4a340a7cc581624cbf7149006f8
florvysin@gmail.com <florvysin@MacBook-Pro-de-Flor.local>
2023-07-24 00:39:38 +0200

    rick

---
+++ src/components/Card.jsx
@@ -0,0 +1,26 @@
+
+
+export default function Card({id, onClose, name, status, species, gender, origin, image}) {
+   return (
+      <div>
+         <button onClick= {onClose} >X</button>
+
+         <h2>{name}</h2>
+         <h2>{id}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin} </h2>
+         <img src ={image} alt='' />
+      </div>
+   );
+} 
+
+
+
+
+
+
+
+
+
